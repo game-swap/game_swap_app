d11bd319d7a70686d9d2cc5cac7cdcc492f2eb38
Liezel Manalo <liezelanne9@gmail.com>
2019-05-08 09:51:43 -0700

    Basic Express server set up

---
+++ gameswap/server/controller.js
@@ -0,0 +1,7 @@
+const db = require('../database/index');
+
+const controller = {
+
+}
+
+module.exports = controller;
---
+++ gameswap/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+require('../client/dist')
+
+const server = express();
+const router = require('./router');
+const port = 3000;
+
+server.use(morgan('dev'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.use('/api', router);
+
+server.use(express.static(path.join(__dirname, '/../client/dist')));
+
+server.listen(port, () => console.log(`connected at ${port}`));
