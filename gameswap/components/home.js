8d50f959cf53f439c0233f8e46b4ff29ee13dbb0
Nate <nathanaelmullins@gmail.com>
2019-05-07 23:34:42 -0700

    [feat] filter by console/search

--- gameswap/components/home.js
+++ gameswap/components/home.js
@@ -8,67 +8,126 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      console: '',
       search: '',
+      console: '',
       games: [
         {
           img:
             'https://images.g2a.com/newlayout/470x470/1x1x0/b99b28590aea/5bc9c521ae653a5fd7677389',
-          title: 'Red Dead Redemption 2'
+          title: 'Red Dead Redemption 2',
+          console: 'XBox One'
         },
         {
           img:
             'https://upload.wikimedia.org/wikipedia/en/thumb/5/5c/Kingdom_Hearts_III_box_art.jpg/220px-Kingdom_Hearts_III_box_art.jpg',
-          title: 'Kingdom Hearts III'
+          title: 'Kingdom Hearts III',
+          console: 'XBox One'
         },
         {
           img:
             'https://www.mobygames.com/images/covers/l/541579-anthem-legion-of-dawn-edition-playstation-4-front-cover.jpg',
-          title: 'Anthem'
+          title: 'Anthem',
+          console: 'XBox One'
         },
         {
           img:
             'https://images.g2a.com/newlayout/470x470/1x1x0/b99b28590aea/5bc9c521ae653a5fd7677389',
-          title: 'Red Dead Redemption 2'
+          title: 'Red Dead Redemption 2',
+          console: 'PS4'
         },
         {
           img:
             'https://upload.wikimedia.org/wikipedia/en/thumb/5/5c/Kingdom_Hearts_III_box_art.jpg/220px-Kingdom_Hearts_III_box_art.jpg',
-          title: 'Kingdom Hearts III'
+          title: 'Kingdom Hearts III',
+          console: 'PS4'
         },
         {
           img:
             'https://www.mobygames.com/images/covers/l/541579-anthem-legion-of-dawn-edition-playstation-4-front-cover.jpg',
-          title: 'Anthem'
+          title: 'Anthem',
+          console: 'PS4'
         },
         {
           img:
             'https://images.g2a.com/newlayout/470x470/1x1x0/b99b28590aea/5bc9c521ae653a5fd7677389',
-          title: 'Red Dead Redemption 2'
+          title: 'Red Dead Redemption 2',
+          console: 'Switch'
         },
         {
           img:
             'https://upload.wikimedia.org/wikipedia/en/thumb/5/5c/Kingdom_Hearts_III_box_art.jpg/220px-Kingdom_Hearts_III_box_art.jpg',
-          title: 'Kingdom Hearts III'
+          title: 'Kingdom Hearts III',
+          console: 'Switch'
         },
         {
           img:
             'https://www.mobygames.com/images/covers/l/541579-anthem-legion-of-dawn-edition-playstation-4-front-cover.jpg',
-          title: 'Anthem'
+          title: 'Anthem',
+          console: 'Switch'
         }
-      ]
+      ],
+      filteredGames: []
     };
   }
 
   updateSearch = search => {
-    this.setState({ search });
+    let filteredGames = this.filterBySearch(search);
+    this.setState({ search, filteredGames });
+  };
+
+  updateConsole = e => {
+    let buttons = ['XBox One', 'PS4', 'Switch'];
+    let filteredGames = this.filterByConsole(buttons[e]);
+    this.setState({ console: buttons[e], filteredGames });
+  };
+
+  clearSearch = () => {
+    if (this.state.console) {
+      let filteredGames = this.filterByConsole(this.state.console);
+      this.setState({ search: '', filteredGames }, () => this.updateSearch);
+    } else {
+      this.setState({ search: '', filteredGames: [] });
+    }
+  };
+
+  filterBySearch = search => {
+    let { games } = this.state;
+    if (this.state.console) {
+      let filteredGames = games.filter(
+        game =>
+          game.title.toLowerCase().includes(search.toLowerCase()) &&
+          game.console === this.state.console
+      );
+      return filteredGames;
+    } else {
+      let filteredGames = games.filter(game =>
+        game.title.toLowerCase().includes(search.toLowerCase())
+      );
+      return filteredGames;
+    }
+  };
+
+  filterByConsole = console => {
+    let { games } = this.state;
+    if (this.state.search) {
+      let filteredGames = games.filter(game =>
+        game.title
+          .toLowerCase()
+          .includes(this.state.search.toLowerCase() && game.console === console)
+      );
+      return filteredGames;
+    } else {
+      let filteredGames = games.filter(game => game.console === console);
+      return filteredGames;
+    }
   };
 
   tradeRequest = () => {};
 
   render() {
-    const { search } = this.state;
-    const buttons = ['XBox One', 'PS4', 'Switch'];
+    let { search, console, games, filteredGames } = this.state;
+    let buttons = ['XBox One', 'PS4', 'Switch'];
+    let gamesToRender = search || console ? filteredGames : games;
     return (
       <View style={{ height: '100%', backgroundColor: '#fffff0' }}>
         <Header
@@ -90,11 +149,13 @@ export default class Home extends Component {
         <SearchBar
           containerStyle={{ backgroundColor: '#fffff0', marginTop: -1 }}
           placeholder="Search for Games"
-          onChange={this.updateSearch}
+          onChangeText={this.updateSearch}
+          onClear={this.clearSearch}
           value={search}
         />
         <ButtonGroup
           buttons={buttons}
+          onPress={this.updateConsole}
           containerStyle={{
             backgroundColor: '#7ed957',
             borderColor: '#000',
@@ -105,7 +166,7 @@ export default class Home extends Component {
         />
         <Text style={styles.text}>Available Games</Text>
         <ScrollView>
-          {this.state.games.map((game, index) => (
+          {gamesToRender.map((game, index) => (
             <Game
               key={index}
               index={index}
