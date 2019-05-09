8940ecbc4246b239c4772b4848514f5c49d0b704
Nate <nathanaelmullins@gmail.com>
2019-05-09 14:53:43 -0700

    [feat] login page and navigation

--- gameswap/App.js
+++ gameswap/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import Login from './components/login.js';
 import Home from './components/home.js';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
-    return (
-      <View>
-        <Home />
-      </View>
-    );
+    return Login;
   }
 }
 
+const AppNavigator = createStackNavigator(
+  {
+    Login: { screen: Login, navigationOptions: { header: null } },
+    Home: { screen: Home, navigationOptions: { header: null } }
+  },
+  {
+    initialRouteName: 'Login',
+    headerMode: 'screen'
+  }
+);
+
 AppRegistry.registerComponent('GameSwap', () => App);
+
+export default createAppContainer(AppNavigator);
