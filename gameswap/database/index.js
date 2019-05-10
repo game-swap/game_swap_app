bc8a0952627cbee01e1f87cc7ef2f47b2cb6f853
Liezel Manalo <liezelanne9@gmail.com>
2019-05-10 15:10:32 -0700

    Fixed conflict

--- gameswap/database/index.js
+++ gameswap/database/index.js
@@ -6,15 +6,9 @@ const sequelize = new Sequelize('gameswap', 'postgres', 'mynameisjeff', {
   pool: { maxConnections: 20, maxIdleTime: 30 }
 });
 
-<<<<<<< HEAD
-sequelize.authenticate({ force: true })
+sequelize.authenticate()
   .then(() => console.log('✔ Connected to Postgres'))
   .catch(err => console.log('✔ unable to connect to postgres', err))
-=======
-sequelize.authenticate()
-  .then(() => console.log('connected to postgres'))
-  .catch(err => console.log('unable to conenct to postgres', err))
->>>>>>> origin/master
 
 
 module.exports = sequelize;
