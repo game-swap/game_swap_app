cb2538e11ecac07963ee7f554eae6a79ad732777
davidfred1994 <davidfred1994@gmail.com>
2019-05-11 10:05:42 -0700

    Fixed untrackeed files

--- gameswap/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        clientID: '642596044589-3feiqm1uf82k6v3k31pe96dtqvkgd4kk.apps.googleusercontent.com',
-        clientSecret: '5QoFqTF6L1XlYuee8d4WRKUD'
-    }
-}
