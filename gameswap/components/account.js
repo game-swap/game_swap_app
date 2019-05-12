2470db645387c8fd459ad448b163c845dd69f9a9
Liezel Manalo <liezelanne9@gmail.com>
2019-05-11 21:16:27 -0700

    Rows and Icons. No onPress functionality yet.

--- gameswap/components/account.js
+++ gameswap/components/account.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Text } from 'react-native';
-import { Header, SearchBar, ButtonGroup } from 'react-native-elements';
+import { StyleSheet, View, ScrollView, Text } from 'react-native';
+import { Header, Button, Icon } from 'react-native-elements';
 // import axios from 'axios';
-// import Logo from './logo.js';
-// import Game from './game.js';
+import Logo from './logo.js';
+import Game from './game.js';
 // import Trade from './trade.js';
 // import Loading from './loading.js';
 
@@ -16,65 +16,161 @@ export default class Account extends Component {
   }
 
   render() {
-    <View>
-      <Header
-        leftComponent={{
-          icon: 'account-circle',
-          color: '#000',
-          marginLeft: '11%'
-        }}
-        centerComponent={
-          <View style={{ display: 'flex', flexDirection: 'row' }}>
-            <Text
-              style={{
-                fontSize: 18,
-                fontWeight: '700',
-                fontFamily: 'Verdana-Bold',
-                color: '#7ed957'
-              }}
-            >
-              Game
-            </Text>
-            <Text
-              style={{
-                fontSize: 18,
-                fontWeight: '700',
-                fontFamily: 'Verdana-Bold',
-                color: '#000'
-              }}
-            >
-              Swap
-            </Text>
-          </View>
-        }
-        rightComponent={<Logo />}
-        containerStyle={{
-          backgroundColor: '#696969',
-          borderBottomColor: '#696969'
-        }}
-      />
+    const styles = StyleSheet.create({
+      listItem1: {
+        width: '100%',
+        height: 180,
+        paddingLeft: 7,
+        paddingTop: 3,
+        paddingBottom: 3,
+        // borderTopWidth: 2,
+        borderBottomWidth: 2,
+        borderColor: '#7ed957',
+        flexDirection: 'row',
+        alignItems: 'center'
+      },
+      listItem: {
+        width: '100%',
+        height: 70,
+        paddingLeft: 7,
+        paddingTop: 3,
+        paddingBottom: 3,
+        borderBottomWidth: 2,
+        borderColor: '#7ed957',
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      textView: { width: "82%", justifyContent: "center" },
+      text: { color: '#d3d3d3', fontSize: 18, fontWeight: '700', paddingTop: 25 },
+      iconView: {
+        width: '20%',
+        height: 77,
+        paddingTop: 16,
+        paddingRight: 7,
+        justifyContent: 'center'
+      }
+    })
+
+    return (
       <View>
-        {/* Welcome User */}
-        <Text>Welcome User</Text>
+        <Header
+          leftComponent={{
+            icon: 'arrow-back',
+            color: '#000',
+            marginLeft: '11%',
+            onPress: () => this.props.navigation.navigate('Home')
+          }}
+          centerComponent={
+            <View style={{ display: 'flex', flexDirection: 'row' }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontWeight: '700',
+                  fontFamily: 'Verdana-Bold',
+                  color: '#7ed957'
+                }}
+              >
+                Game
+              </Text>
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontWeight: '700',
+                  fontFamily: 'Verdana-Bold',
+                  color: '#000'
+                }}
+              >
+                Swap
+              </Text>
+            </View>
+          }
+          rightComponent={<Logo />}
+          containerStyle={{
+            backgroundColor: '#696969',
+            borderBottomColor: '#696969'
+          }}
+        />
+        <View style={{ height: '100%', backgroundColor: '#141414' }}>
+
+          <View style={styles.listItem1}>
+
+            <View style={styles.textView}>
+              <Text style={styles.text}>
+                Welcome User
+              </Text>
+            </View>
+            <View style={styles.iconView}>
+              <Icon
+                name='account-circle'
+                size={60}
+              />
+            </View>
+
+          </View>
+
+          <ScrollView>
+
+            <View> 
+
+              <View style={styles.listItem}>
+                <View style={styles.textView}>
+                  <Text style={styles.text}>
+                    Change Username
+                  </Text>
+                </View>
+                <View style={styles.iconView}>
+                  <Icon 
+                    name='arrow-drop-down-circle'
+                    color='#7ed957'
+                  />
+                </View>
+              </View>
+              <View style={styles.listItem}> 
+                <View style={styles.textView}>
+                  <Text style={styles.text}>
+                    Change Email
+                  </Text>
+                </View>
+                <View style={styles.iconView}>
+                  <Icon 
+                    name='arrow-drop-down-circle'
+                    color='#7ed957'
+                  />
+                </View>
+              </View>
+              <View style={styles.listItem}>
+                <View style={styles.textView}>
+                  <Text style={styles.text}>
+                    Change Password
+                  </Text>
+                </View>
+                <View style={styles.iconView}>
+                  <Icon 
+                    name='arrow-drop-down-circle'
+                    color='#7ed957'
+                  />
+                </View>
+              </View>
+
+            </View>
+
+            <View style={styles.listItem}>
+              <View style={styles.textView}>
+                <Text style={styles.text}>
+                  Add/Remove Games For Offer
+                </Text>
+              </View>
+              <View style={styles.iconView}>
+                <Icon 
+                  name='arrow-drop-down-circle'
+                  color='#7ed957'
+                />
+              </View>
+            </View>
+
+          </ScrollView>
+        </View>
       </View>
-      <ScrollView>
-        <View>
-          {/* change username */}
-          <Text>Change Username</Text>
-        </View>
-        <View>
-          {/* change email */}
-          <Text>Change Email</Text>
-        </View>
-        <View>
-          {/* change password */}
-          <Text>Change Password</Text>
-        </View>
-        <View>
-          {/* Add/Remove Games For Offer */}
-          <Text>Add/Remove Games For Offer</Text>
-        </View>
-      </ScrollView>
-    </View>
+    )
   }
 }
--- gameswap/components/home.js
+++ gameswap/components/home.js
@@ -15,12 +15,17 @@ export default class Home extends Component {
       search: '',
       console: '',
       consoleIndex: null,
-      consoleRequest: '',
       trade: false,
+      game_id: '',
+      gameName: '',
+      consoleRequest: '',
       gameIndex: '',
       games: [],
-      filteredGames: []
+      filteredGames: [],
+      offeredGames: []
     };
+    this.setRequestedGame = this.setRequestedGame.bind(this);
+    this.addOfferedGame = this.addOfferedGame.bind(this);
   }
 
   componentWillMount() {
@@ -30,13 +35,40 @@ export default class Home extends Component {
       .catch(err => console.log(err));
   }
 
+  sendRequest() {
+    const { game_id, gameName, consoleRequest, offeredGames } = this.state;
+    const platformCodes = { PS4: '48', 'XBox One': '49', Switch: '130' };
+    axios.post('18.220.172.51:3000/api/email', {
+      buyer: 'Julian Yuen',
+      user_id: 3,
+      game: gameName,
+      game_id: game_id,
+      platform_id: platformCodes[consoleRequest],
+      offer: offeredGames,
+      email: 'therealspiderman@hr.com'
+    });
+  }
+
+  setRequestedGame = game => {
+    const { game_id, name } = game;
+    this.setState({ game_id, gameName: name });
+  };
+
+  addOfferedGame = game => {
+    const { offeredGames } = this.state;
+    offeredGames.includes(game.name)
+      ? offeredGames.push(game.name)
+      : offeredGames.filter(gameName => gameName !== game.name);
+    this.setState({ offeredGames });
+  };
+
   updateSearch = search => {
-    let filteredGames = this.filterBySearch(search);
+    const filteredGames = this.filterBySearch(search);
     this.setState({ search, filteredGames });
   };
 
   updateConsole = consoleIndex => {
-    let buttons = ['XBox One', 'PS4', 'Switch'];
+    const buttons = ['XBox One', 'PS4', 'Switch'];
     if (consoleIndex === this.state.consoleIndex) {
       this.setState(
         { console: '', consoleIndex: null, consoleRequest: '' },
@@ -48,7 +80,7 @@ export default class Home extends Component {
         }
       );
     } else {
-      let filteredGames = this.filterByConsole(buttons[consoleIndex]);
+      const filteredGames = this.filterByConsole(buttons[consoleIndex]);
       this.setState({
         console: buttons[consoleIndex],
         consoleRequest: buttons[consoleIndex],
@@ -74,8 +106,8 @@ export default class Home extends Component {
   };
 
   filterBySearch = search => {
-    let { games } = this.state;
-    let platformCodes = { PS4: '48', 'XBox One': '49', Switch: '130' };
+    const { games } = this.state;
+    const platformCodes = { PS4: '48', 'XBox One': '49', Switch: '130' };
     if (this.state.console) {
       let filteredGames = games.filter(
         game =>
@@ -92,8 +124,8 @@ export default class Home extends Component {
   };
 
   filterByConsole = console => {
-    let { games, search } = this.state;
-    let platformCodes = { PS4: '48', 'XBox One': '49', Switch: '130' };
+    const { games, search } = this.state;
+    const platformCodes = { PS4: '48', 'XBox One': '49', Switch: '130' };
     if (search) {
       let filteredGames = games.filter(
         game =>
@@ -141,6 +173,7 @@ export default class Home extends Component {
               visible={this.state.trade}
               close={this.closeOverlay}
               updateConsoleRequest={this.updateConsoleRequest}
+              addOfferedGame={this.addOfferedGame}
               console={consoleRequest}
               game={
                 filteredGames.length
@@ -149,7 +182,12 @@ export default class Home extends Component {
               }
             />
             <Header
-              leftComponent={<Logo />}
+              leftComponent={{
+                icon: 'account-circle',
+                color: '#000',
+                marginLeft: '11%',
+                onPress: () => this.props.navigation.navigate('Account')
+              }}
               centerComponent={
                 <View style={{ display: 'flex', flexDirection: 'row' }}>
                   <Text
@@ -174,10 +212,7 @@ export default class Home extends Component {
                   </Text>
                 </View>
               }
-              rightComponent={{
-                icon: 'account-circle',
-                color: '#000'
-              }}
+              rightComponent={<Logo />}
               containerStyle={{
                 backgroundColor: '#696969',
                 borderBottomColor: '#696969'
@@ -215,9 +250,9 @@ export default class Home extends Component {
                   <Game
                     key={index}
                     index={index}
-                    cover={game.cover}
-                    name={game.name}
+                    game={game}
                     tradeRequest={this.tradeRequest}
+                    setRequestedGame={this.setRequestedGame}
                   />
                 ))}
               </ScrollView>
